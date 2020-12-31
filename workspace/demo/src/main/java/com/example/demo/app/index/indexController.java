f8fb552af8afd14700187dcc436ecdf3f7956bd5
kato_m <3113022@gmail.com>
2020-12-31 22:43:55 +0900

    dmo commit

--- workspace/demo/src/main/java/com/example/demo/app/index/indexController.java
+++ workspace/demo/src/main/java/com/example/demo/app/index/indexController.java
@@ -4,10 +4,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
-@RequestMapping("/")
 public class indexController {
-  @GetMapping
-  public String index(Model model) {
-    return "index";
-  }
+	@GetMapping
+	public String index(Model model) {
+		return "index";
+	}
 }
